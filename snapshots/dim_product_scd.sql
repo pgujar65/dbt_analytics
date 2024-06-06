97eb169d82bc11ba92a7201cd9c13bb8ca750a0c
Punit Gujar <punitgujar111@gmail.com>
2024-06-06 19:12:33 +0000

    SCD2 type implementation.

---
+++ snapshots/dim_product_scd.sql
@@ -0,0 +1,17 @@
+{% snapshot dim_product_snapshot %}
+
+{{
+    config(
+      target_database='analytics-424809',
+      target_schema='GDH_GOLD',
+      unique_key='product_key',
+
+      strategy='check',
+      check_cols=['short_product_desc', 'long_product_desc', 'alcohol_type', 'rec_crt_dt'],  
+      updated_at='rec_crt_dt',
+    )
+}}
+
+select * from {{ source('GDH_SILVER', 'dim_product') }}
+
+{% endsnapshot %}
---
+++ snapshots/dim_store_scd.sql
@@ -0,0 +1,17 @@
+{% snapshot dim_store_snapshot %}
+
+{{
+    config(
+      target_database='analytics-424809',
+      target_schema='GDH_GOLD',
+      unique_key='store_key',
+
+      strategy='check',
+      check_cols=['store_name'],  
+      updated_at='rec_crt_dt',
+    )
+}}
+
+select * from {{ source('GDH_SILVER', 'dim_store') }}
+
+{% endsnapshot %}
